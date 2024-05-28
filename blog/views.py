8dfd38e70d0b40bdf3b32bd320359fd62ef9bc6b
Ramiro Serrano <ramiroivanserrano@gmail.com>
2024-05-28 17:14:19 -0300

    Agregando login y comentarios

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,33 @@
 #importar utilidades
 from django.shortcuts import render,redirect, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 #importar modelos
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
+
+#Vistas de autenticacion
+# def login(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         redirect('/')
+#     else:
+#         return 'invalid login'
+    
+
+# def  logout_view(request):
+#     logout(request)
+#     redirect('/')
+
+
+
+#Vistas de Blog
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
@@ -14,7 +36,7 @@ def post_detail(request, pk):
     post = Post.objects.get(pk = pk)
     return render(request, 'blog/post_detail.html', {'post': post})
     
-
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -28,6 +50,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post =  get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -41,3 +64,45 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
--- blogweb/settings.py
+++ blogweb/settings.py
@@ -12,8 +12,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+#Redirect to index
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
+
+
 
 
 # Quick-start development settings - unsuitable for production
